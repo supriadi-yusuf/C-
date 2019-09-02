69a7d5e3c11aa97a08b5efc74cbbada02abb8476
supriadi-yusuf <supriadi96@gmail.com>
2019-09-02 21:36:13 +0700

    mofied thread

--- thread-5.cpp
+++ thread-5.cpp
@@ -14,13 +14,24 @@ g++ thread-5.cpp -pthread -std=gnu++11 -o thread-5
 using namespace std;
  
 void threadFn(){
-  cout << "hello ... from inside thread function" << endl;
+  cout << "inside threadFn function\n";
+}
+
+void threadFn2(int value){
+  cout << "inside threadFn2 function, value = " << value << endl;
+}
+
+void threadFn3(int& value){
+  cout << "inside threadFn3 function, value = " << value << endl;
+  value++;
 }
 
 int main(int argc, char* argv[]){
   thread t1(threadFn);
+  thread t2{threadFn};
 
   t1.join(); 
+  t2.join(); 
 
   return 0;
 }
